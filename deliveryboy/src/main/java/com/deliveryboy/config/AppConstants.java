631e5bb333d03e059cb62abe7eff70c003d94f21
sayan289 <sayanadhikary289@gmail.com>
2024-04-15 14:21:12 +0530

    Kafka

---
+++ deliveryboy/src/main/java/com/deliveryboy/config/AppConstants.java
@@ -0,0 +1,7 @@
+package com.deliveryboy.config;
+
+import org.apache.kafka.common.protocol.types.Field;
+
+public class AppConstants {
+    public static final String LOCATION_TOPIC_NAME="location-update-topic";
+}
